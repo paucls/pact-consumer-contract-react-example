ba37b737d3ddc4503deb9f1a5c6de44c3cb2e0d9
Esteban, Paulo <Paulo.Esteban@emc.com>
2017-02-05 11:33:48 +0000

    Added fixture to be used by our tests

---
+++ src/fixtures/feedback-client.js
@@ -0,0 +1,9 @@
+const feedbackOne = {
+    category: 'Question',
+    title: 'Export projects',
+    message: 'Is it possible to export projects on this app?'
+};
+
+export const feedbacksFixtures = {
+    getFeedback: feedbackOne
+}
