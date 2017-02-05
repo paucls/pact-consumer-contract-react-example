fba71ba49cc3c5cca7e47241d7ec336b0750ec7a
Esteban, Paulo <Paulo.Esteban@emc.com>
2017-02-05 16:13:37 +0000

    Using Axios to implement http client

--- src/FeedbackClient.js
+++ src/FeedbackClient.js
@@ -1,10 +1,18 @@
+import axios from "axios";
+
 class FeedbackClient {
     constructor(options) {
         this.host = options.host;
     }
 
     create(feedback) {
-        return feedback;
+        const headers = {"Accept": "application/json"};
+
+        return axios.post(`${this.host}/events`, feedback, headers)
+            .then(function (response) {
+                return response.data;
+            });
+
     }
 }
 
